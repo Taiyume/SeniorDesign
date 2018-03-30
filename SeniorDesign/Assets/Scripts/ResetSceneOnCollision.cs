41ec15203feea7e42dcd33fe63d82fc06c31eae4
Taiyume <aklomkaew@gmail.com>
2018-03-29 22:05:44 -0500

    Finished item placements; added reset buttons, text, etc.

---
+++ SeniorDesign/Assets/Scripts/ResetSceneOnCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetSceneOnCollision : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void OnCollisionEnter (Collision collision) {
+            SceneManager.LoadScene("TestEnvironment_Jaylyn");
+        }
+	}
+
